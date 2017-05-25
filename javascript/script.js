07abd4e8242ba8e21fd4476703410621025a6bf0
Marcel Fahle <m.fahle@gmail.com>
2017-05-25 16:22:09 +0300

    adds dropdown

--- javascript/script.js
+++ javascript/script.js
@@ -6,4 +6,22 @@ $(document).ready( function() {
   });
 
 
+  // determine X position of primary nav.
+  // needed because dropdown menus tend 
+  // tend to be wider than it's natural parent,
+  // .nav__primary--item and to get a flexible
+  // width based on content it can't be placed 
+  // in there (using relative/absolute relationship)
+  // But that way we don't know where the menu has 
+  // to be placed on the X-axis as it looses it's 
+  // positional relationship to .nav__primary--item
+  // that's why we need this little JS hack to read
+  // the positions of the menu. It'll work without JS,
+  // but then it always positions the dropdown at the
+  // far left, which is not a problem IMHO with those big things. :-)
+
+  $('.nav__primary--item').each(function(i, el) {
+    $(el).find('.dropdown-menu').css( 'left', (el.offsetLeft-7) + 'px');
+  });
+
 });
